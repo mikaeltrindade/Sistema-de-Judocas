1688728a82ca4ecda68ddcac3fe4c8c5c276f0db
Mikael Batista da Trindade <mikaelbatistadatrindade@Mikaels-MacBook-Air.local>
2024-11-25 13:28:12 -0300

    Correção de arquitetura das pastas com o maven

--- src/java/org/fpij/jitakyoei/model/validator/AlunoValidator.java
+++
@@ -1,9 +0,0 @@
-package main.java.org.fpij.jitakyoei.model.validator;
-
-import main.java.org.fpij.jitakyoei.model.beans.Aluno;
-
-public class AlunoValidator implements Validator<Aluno>{
-	public boolean validate(Aluno obj) {
-		return true;
-	}
-}
--- src/java/org/fpij/jitakyoei/view/AppView.java
+++
@@ -1,9 +0,0 @@
-package main.java.org.fpij.jitakyoei.view;
-
-import main.java.org.fpij.jitakyoei.facade.AppFacade;
-
-public interface AppView {
-	public void handleModelChange(Object obj);
-	public void displayException(Exception e);
-	public void registerFacade(AppFacade facade);
-}
